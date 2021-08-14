e04255ef0e14a3700c431b6cc7a0b75c77ef92b5
unknown <paulohnq55@outlook.com>
2021-08-14 15:34:09 -0300

    finish

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank
+{
+    class ContaCorrente
+    {
+        public string Titular { get; set; }
+        private double _saldo = 0;
+        public double Saldo
+        {
+            get {
+                return _saldo;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _saldo = value;
+                }
+            }
+
+        }
+        public int Agencia { get; }
+        public int NumeroDaConta{get;}
+        public static int _qtdeDeContas { get; private set; }
+        public static double TaxaOperacao { get; private set; }
+        
+        public ContaCorrente(string titular,int numeroDaConta,int agencia)
+        {
+           
+            if (agencia<=0)
+            {  
+             //lançador
+                throw new ArgumentException("O argumento agencia deve ser maior que 0");
+            }
+            if (numeroDaConta<=0)
+            {
+                throw new ArgumentException("O argumento numeroDaConta deve ser maior que 0");
+            }
+
+
+            Titular = titular;
+            NumeroDaConta = numeroDaConta;
+            Agencia = agencia;
+
+           _qtdeDeContas++;
+        }
+
+        public static  int QuantidadeDeContas()
+        {
+            return _qtdeDeContas;
+        }
+        public bool Depositar(double valorDeposito)
+        {
+            if (valorDeposito<=0)
+            {
+                Console.WriteLine("Valor inválido");
+                return false;
+            }
+            else { 
+                 Saldo += valorDeposito;
+                return true;
+            }
+            
+        }
+        public bool Sacar(double valorSaque)
+        {
+            if (Saldo < valorSaque)
+            {
+                Console.WriteLine("Saldo insuficiente para saque, seu saldo é: " + Saldo);
+                return false;
+            }
+            else
+            { 
+                Saldo -= valorSaque;
+                return true;
+            }
+             
+        }
+    }
+   
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                ContaCorrente conta1 = new ContaCorrente("PAULO HENRIQUE SILVA DOS SANTOS",1160397,1234);
+            }
+            //os caths mais específicos devem vir primeiro
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException ");
+                Console.WriteLine(ex.Message);
+
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+           
+        }
+    }
+}
